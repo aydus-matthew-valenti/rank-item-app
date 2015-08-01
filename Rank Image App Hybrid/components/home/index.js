a62ad82daf6faf66059ee93bdddcde38e118f4e3
Matthew Valenti <matthew@aydus.com>
2015-08-01 21:56:07 +0000

    Changes from Screen Builder

--- Rank Image App Hybrid/components/home/index.js
+++ Rank Image App Hybrid/components/home/index.js
@@ -2,14 +2,19 @@
 
 app.home = kendo.observable({
     onShow: function() {
-		        alertok();
-        function alertok() {
-    	alert('ok');
+        
+        var items = everlive.data('Items');
+items.get(null, function(data) {
+    alert(JSON.stringify(data));
+}, function(err) {
+    alert(err.message);
+});
+
+        
 	}
-    }
-    
-    
 });
 
 // START_CUSTOM_CODE_home
+
+
 // END_CUSTOM_CODE_home
