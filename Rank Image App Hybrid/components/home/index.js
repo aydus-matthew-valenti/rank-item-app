3e988c58ee7b6658541cbe4fc30c022db7e7c77d
Matthew Valenti <matthew@aydus.com>
2015-08-02 00:43:33 +0000

    Changes from Screen Builder

--- Rank Image App Hybrid/components/home/index.js
+++ Rank Image App Hybrid/components/home/index.js
@@ -4,25 +4,20 @@ app.home = kendo.observable({
     
     imgSrc1: 'https://img0.etsystatic.com/004/0/6762460/il_570xN.391729514_i79d.jpg',
     
-    onShow: function() {}
+    onShow: function() {},
     
-});
-
-(function(parent) {
-})(app.home);
-
-
-
-// START_CUSTOM_CODE_home
-document.addEventListener("deviceready", function() {
-
-    function getRandom(count) {
-
+    init: function() {
+        this.setRandom(this.imgSrc1);
+    },
+    
+    setRandom(property) {
+        
         var everlive = window.everlive;
         var items = everlive.data('Items');
 
         items.count()
             .then(function(data) {
+            alert(data)
                     var count = data.result
                     var query = new Everlive.Query();
 
@@ -30,7 +25,7 @@ document.addEventListener("deviceready", function() {
                     items.get(query)
                         .then(function(data) {
                                 alert(JSON.stringify(data));
-                                //return data;
+                                //property = data;
                             },
                             function(error) {
                                 alert(JSON.stringify(error));
@@ -41,4 +36,7 @@ document.addEventListener("deviceready", function() {
                 });
     }
 });
+
+// START_CUSTOM_CODE_home
+app.home.init();
 // END_CUSTOM_CODE_home
